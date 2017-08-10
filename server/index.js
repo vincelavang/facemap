9b8e2b4017a7799f8bb174ccf72cec8a68767392
Vince Lavang <vincelavang@gmail.com>
2017-08-10 11:38:11 -0700

    Have app listen on 3000. #2

--- server/index.js
+++ server/index.js
@@ -5,3 +5,7 @@ const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
 
 app.use(staticMiddleware)
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!')
+})
